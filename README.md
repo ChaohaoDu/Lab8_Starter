3d0bd52abc3fed02626a5629daa007a27217a2ca
Chaohao Du <chaohaodu@gmail.com>
2022-11-18 02:57:08 -0800

    complete

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Lab 8 - Starter
+Chaohao Du
+
+1. Answer: 1.
+   In the second paragraph of the `intro`, it says
+   > automation testing...code getting pushed to the remote repository, or test runs of the whole repo that are
+   triggered nightly.
+
+2. Answer: No.
+   End to end test emulates user actions. Users don't care about a specific fucntion.
+3. Answer: No.
+   Message feature requires a user to send a message to another, which is can't be completed by a unit test.
+4. Answer: Yes.
+   Unit test can check the length of the text that user write.
+
+### Screenshots
+![](assets/screenshots/img_4.png)
+![](assets/screenshots/img_2.png)
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -1,5 +1,86 @@
 // unit.test.js
 
-const functions = require('../code-to-unit-test/unit-test-me.js');
+const {isPhoneNumber, isEmail, isStrongPassword, isDate, isHexColor} = require("../code-to-unit-test/unit-test-me");
 
 // TODO - Part 2
+test('phone number true 1', () => {
+   expect(isPhoneNumber('1')).toBe(false);
+})
+
+test('phone number true 2', () => {
+   expect(isPhoneNumber('111-111-1111')).toBe(true);
+})
+
+test('phone number false 1', () => {
+   expect(isPhoneNumber('1')).toBe(true);
+})
+
+test('phone number false 2', () => {
+   expect(isPhoneNumber('111-111-1111')).toBe(false);
+})
+
+test('email true 1', () => {
+   expect(isEmail("chaohaodu@gmail.com")).toBe(true);
+})
+
+test('email true 2', () => {
+   expect(isEmail("du.gmail.com")).toBe(false);
+})
+
+test('email false 1', () => {
+   expect(isEmail("chaohaodu@gmail.com")).toBe(false);
+
+})
+
+test('email false 2', () => {
+   expect(isEmail("du.gmail.com")).toBe(true);
+
+})
+
+test('strong password true 1', () => {
+   expect(isStrongPassword('abcd123')).toBe(true);
+})
+
+test('strong password true 2', () => {
+   expect(isStrongPassword('123abc')).toBe(false);
+})
+
+test('strong password false 1', () => {
+   expect(isStrongPassword('abcdefghijklmnopqrstuvwxyz')).toBe(true);
+})
+
+test('strong password false 2', () => {
+   expect(isStrongPassword('a1d2f3fg4')).toBe(false);
+})
+
+test('date true 1', () => {
+   expect(isDate('01/02/2022')).toBe(true);
+})
+
+test('date true 2', () => {
+   expect(isDate('01.02.2022')).toBe(false);
+})
+
+test('date false 1', () => {
+   expect(isDate('013/02/2022')).toBe(true);
+})
+
+test('date false 2', () => {
+   expect(isDate('01/02/9999')).toBe(false);
+})
+
+test('hex true 1', () => {
+   expect(isHexColor('#000000')).toBe(true);
+})
+
+test('hex true 1', () => {
+   expect(isHexColor('$000000')).toBe(false);
+})
+
+test('hex false 1', () => {
+   expect(isHexColor('#3fa')).toBe(false);
+})
+
+test('hex false 2', () => {
+   expect(isHexColor('#12345678')).toBe(true);
+})
